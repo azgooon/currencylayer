d01c493e902bc356e3730f506160d8bd636e5e7a
Orkhan Ahmadov <ahmadov90@gmail.com>
2019-09-20 15:41:20 +0200

    WIP

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,32 @@
 
 namespace Orkhanahmadov\Currencylayer;
 
-class Client
+interface Client
 {
+    /**
+     * @param string $sourceCurrency
+     *
+     * @return $this
+     */
+    public function source(string $sourceCurrency): self;
+
+    /**
+     * @param array<string>|string $currencies
+     *
+     * @return $this
+     */
+    public function currencies($currencies): self;
+
+    /**
+     * @return array
+     */
+    public function live(): array;
+
+//    public function date($currencies): self;
+//
+//    public function historical(): array;
+//
+//    public function convert(float $amount, string $fromCurrency, string $toCurrency);
+//
+//    public function currencies(): array;
 }
---
+++ src/CurrencylayerClient.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Orkhanahmadov\Currencylayer;
+
+use GuzzleHttp\Client as Guzzle;
+
+class CurrencylayerClient implements Client
+{
+    /**
+     * @var Guzzle
+     */
+    private $client;
+    /**
+     * @var string
+     */
+    private $accessKey;
+    /**
+     * @var string
+     */
+    private $source;
+    /**
+     * @var string
+     */
+    private $currencies;
+
+    /**
+     * CurrencylayerClient constructor.
+     *
+     * @param string $accessKey
+     * @param bool $useHttps
+     */
+    public function __construct(string $accessKey, bool $useHttps = false)
+    {
+        $this->client = new Guzzle([
+            'base_uri' => $useHttps ? 'https://apilayer.net/api/' : 'http://apilayer.net/api/'
+        ]);
+
+        $this->accessKey = $accessKey;
+    }
+
+    /**
+     * @param string $source
+     *
+     * @return $this
+     */
+    public function source(string $source): Client
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    /**
+     * @param array<string>|string $currencies
+     *
+     * @return $this
+     */
+    public function currencies($currencies): Client
+    {
+        if (is_array($currencies)) {
+            $this->currencies = implode(',', $currencies);
+        } else {
+            $this->currencies = $currencies;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function live(): array
+    {
+        return $this->request('live', [
+            'currencies' => $this->currencies,
+            'source' => $this->source,
+        ]);
+    }
+
+    /**
+     *
+     *
+     * @param string $endpoint
+     * @param array $query
+     *
+     * @return array
+     */
+    private function request(string $endpoint, array $query): array
+    {
+        $response = $this->client->get($endpoint, [
+            'query' => array_merge($query, ['access_key' => $this->accessKey]),
+        ]);
+
+        $data = \GuzzleHttp\json_decode($response->getBody()->getContents());
+
+        if (array_key_exists('error', $data)) {
+            throw new \InvalidArgumentException($data['error']['info'], $data['error']['code']);
+        }
+
+        return $data;
+    }
+}
